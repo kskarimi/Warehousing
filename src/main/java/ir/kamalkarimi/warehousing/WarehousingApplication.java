125759d5a9c3d52fab42530e86de2fcf6bb31c39
kkarimi <jdG35@6gdFGS3Hdj>
2021-05-28 13:22:13 +0430

    Start creating code base.

---
+++ src/main/java/ir/kamalkarimi/warehousing/WarehousingApplication.java
@@ -0,0 +1,13 @@
+package ir.kamalkarimi.warehousing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehousingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehousingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/kamalkarimi/warehousing/WarehousingApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.kamalkarimi.warehousing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehousingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
