76fa86a58dde6a0b6648c8af0be13e28b71f39b3
kkarimi <jdG35@6gdFGS3Hdj>
2021-05-29 23:37:51 +0430

    some changes!

--- src/main/java/ir/kamalkarimi/warehousing/dto/ProductDto.java
+++ src/main/java/ir/kamalkarimi/warehousing/dto/ProductDto.java
@@ -3,10 +3,19 @@ package ir.kamalkarimi.warehousing.dto;
 import java.util.List;
 
 public class ProductDto {
+    private Long   id;
     private String name;
     private List<ArticleDto> articles;
 
     public ProductDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
